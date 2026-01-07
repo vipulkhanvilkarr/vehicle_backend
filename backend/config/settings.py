f23db9163ce98b654099d7ebe8b8a908c2acd4dd
vipul khanvilkar <110216542+vipulkhanvilkarr@users.noreply.github.com>
2026-01-07 23:39:25 +0530

    update req and settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,6 +122,8 @@ DATABASES = {
     }
 }
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
